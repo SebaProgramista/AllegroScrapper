f40b907857e9a9a688e3dc1bb475f2e5a1b022c0
Sheen <mrprofessoracc@gmail.com>
2022-11-12 11:00:38 +0100

    Get data from JSON

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import json
 import selenium
+
+# get data from config file
+with open('config.json', 'r') as f:
+    data = json.load(f)
+
+# const variables
+SEARCH_ITEM = data["inputs"]["SEARCH_ITEM"]
